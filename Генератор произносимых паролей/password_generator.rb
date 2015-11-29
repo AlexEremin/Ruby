3da0d2e902b0c6716d3c857265c9439522f3b480
Andrew <murkakurka@gmail.com>
2015-11-29 15:46:57 +0300

    some code

---
+++ Генератор произносимых паролей/password_generator.rb
@@ -0,0 +1,15 @@
+# Практическое задание 0
+# Простой генератор произносимых паролей
+
+#Пока еще не очень произносимых
+
+SIZE = 5
+
+alphabet = ('a'..'z').to_a | ('A'..'Z').to_a
+
+password = []
+SIZE.times do
+  password << alphabet[rand(alphabet.size)]
+end
+
+print password
